04703618eef65864e41edc2696134ad14bf621a3
bhurtelmahesh <bhurtelace@gmail.com>
2022-03-26 18:26:50 +0900

    css things

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,13 @@ import React from 'react';
 const Footer = () => {
     return(
         <footer className='footer'>
-            <p>&copy; 2022 Bhurtel Development. All  rights reserved.</p>
+            <ul>
+                <li>facebook</li>
+                <li>twitter</li>
+                <li>github</li>
+                <li>youtube</li>
+            </ul>
+                <p>&copy; 2022 Bhurtel Development. All  rights reserved.</p>
         </footer>
     )
 }
